5e14c1c5968651fd2f5ca6da282d1c2b2f82fe2d
Andres Mesa <andresf.mesad@gmail.com>
2020-06-19 22:06:50 -0500

    Secure Password complete

--- src/main.py
+++ src/main.py
@@ -1,12 +1,32 @@
 # Resolve the problem!!
 import string
+import random
 
 SYMBOLS = list('!"#$%&\'()*+,-./:;?@[]^_`{|}~')
-
+#Incluyo minusculas, mayusculas y digitos.
+lower = string.ascii_lowercase
+upper = string.ascii_uppercase
+digits = string.digits
 
 def generate_password():
-    # Start coding here
+    
+    #Genero un entero de 2 a 4, para que la contraseña tome de a 2 a max 4  items de cada lista
+    items = random.randint(2,4)
+    #Genero la contraseña vacía para luego añadirle los caracteres
+    password = ""
 
+    #Creo una lista para cada uno (min, mayus, digits, symbols)
+    aleatoriosMinus = random.sample(lower, items)
+    aleatoriosMayus = random.sample(upper, items)
+    aleatoriosSymbols = random.sample(SYMBOLS, items)
+    aleatoriosDigits = random.sample(digits, items)
+    #Creo una lista que contenga las cuatro listas anteriores
+    passwordlist = aleatoriosMinus + aleatoriosMayus + aleatoriosSymbols + aleatoriosDigits
+
+    #Inserto la lista como texto en la password vacía que tenia anteriormente
+    password = password.join(passwordlist)
+
+    return password
 
 def validate(password):
 
